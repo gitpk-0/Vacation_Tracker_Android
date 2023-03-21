e005dc5d1f8c101d3eda55cf88c0a01b7e9cbe13
gitpk-0 <kell108p@protonmail.com>
2023-03-21 19:44:05 -0400

    B1 - DAO

---
+++ app/src/main/java/wgu/patrick_kell_d308/DAO/ExcursionDAO.java
@@ -0,0 +1,34 @@
+package wgu.patrick_kell_d308.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import wgu.patrick_kell_d308.Entities.Excursion;
+
+/**
+ * @author Patrick Kell
+ */
+@Dao
+public interface ExcursionDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Excursion excursion);
+
+    @Update
+    void update(Excursion excursion);
+
+    @Delete
+    void delete(Excursion excursion);
+
+    @Query("SELECT * FROM excursions ORDER BY excursionID ASC")
+    List<Excursion> getAllExcursions();
+
+    @Query("SELECT * FROM excursions WHERE vacationID= :vacationID ORDER BY excursionID ASC")
+    List<Excursion> getAllExcursions(int vacationID);
+}
