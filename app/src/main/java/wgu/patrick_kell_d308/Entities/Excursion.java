49d3d3715a07a538092ff05f52c5d8743807c37e
gitpk-0 <kell108p@protonmail.com>
2023-03-21 19:35:11 -0400

    B1 - Entities (excursion update)

--- app/src/main/java/wgu/patrick_kell_d308/Entities/Excursion.java
+++ app/src/main/java/wgu/patrick_kell_d308/Entities/Excursion.java
@@ -1,6 +1,7 @@
 package wgu.patrick_kell_d308.Entities;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 /**
  * @author Patrick Kell
@@ -9,16 +10,29 @@ import androidx.room.Entity;
 @Entity(tableName = "excursions")
 public class Excursion {
 
+    @PrimaryKey(autoGenerate = true)
+    private int excursionID;
     private String excursionTitle;
     private String excursionDate;
+    private int vacationID;
 
-    public Excursion(String excursionTitle, String excursionDate) {
+    public Excursion(int excursionID, String excursionTitle, String excursionDate, int vacationID) {
+        this.excursionID = excursionID;
         this.excursionTitle = excursionTitle;
         this.excursionDate = excursionDate;
+        this.vacationID = vacationID;
     }
 
     // public Excursion() {}
 
+
+    public int getExcursionID() {
+        return excursionID;
+    }
+
+    public void setExcursionID(int excursionID) {
+        this.excursionID = excursionID;
+    }
 
     public String getExcursionTitle() {
         return excursionTitle;
@@ -34,5 +48,13 @@ public class Excursion {
 
     public void setExcursionDate(String excursionDate) {
         this.excursionDate = excursionDate;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
     }
 }
