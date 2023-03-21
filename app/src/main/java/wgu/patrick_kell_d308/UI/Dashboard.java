260f8edeef5451823e6a7b542984645ff44b5f79
gitpk-0 <kell108p@protonmail.com>
2023-03-21 15:53:02 -0400

    B1 - Welcome Screen complete, start Dashboard

---
+++ app/src/main/java/wgu/patrick_kell_d308/UI/Dashboard.java
@@ -0,0 +1,16 @@
+package wgu.patrick_kell_d308.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import wgu.patrick_kell_d308.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/wgu/patrick_kell_d308/UI/MainActivity.java
+++ app/src/main/java/wgu/patrick_kell_d308/UI/MainActivity.java
@@ -1,7 +1,11 @@
-package wgu.patrick_kell_d308;
+package wgu.patrick_kell_d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
+
+import wgu.patrick_kell_d308.R;
 
 public class MainActivity extends AppCompatActivity {
 
