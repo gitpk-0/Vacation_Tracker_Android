d11fbb8cce35b95476696dce10b05d816ebb66e1
gitpk-0 <kell108p@protonmail.com>
2023-03-21 16:02:51 -0400

    B1 - launch Dashboard

--- app/src/main/java/wgu/patrick_kell_d308/UI/MainActivity.java
+++ app/src/main/java/wgu/patrick_kell_d308/UI/MainActivity.java
@@ -1,8 +1,12 @@
 package wgu.patrick_kell_d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import wgu.patrick_kell_d308.R;
@@ -13,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchDashboard(View v) {
+        Intent dashboard = new Intent(MainActivity.this, Dashboard.class);
+        startActivity(dashboard);
     }
 }
