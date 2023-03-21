1398e7a3a741c17e11137fd2c00739658b44bd02
gitpk-0 <kell108p@protonmail.com>
2023-03-21 19:23:20 -0400

    B1 - Entities created, build gradle updated

--- app/src/main/java/wgu/patrick_kell_d308/Entities/Excursion.java
+++ app/src/main/java/wgu/patrick_kell_d308/Entities/Excursion.java
@@ -1,4 +1,11 @@
 package wgu.patrick_kell_d308.Entities;
 
+import androidx.room.Entity;
+
+/**
+ * @author Patrick Kell
+ */
+
+@Entity(tableName = "excursions")
 public class Excursion {
 }
--- app/src/main/java/wgu/patrick_kell_d308/Entities/Vacation.java
+++ app/src/main/java/wgu/patrick_kell_d308/Entities/Vacation.java
@@ -1,4 +1,21 @@
 package wgu.patrick_kell_d308.Entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+/**
+ * @author Patrick Kell
+ */
+@Entity(tableName = "vacations")
 public class Vacation {
+
+    @PrimaryKey(autoGenerate = true)
+    private int vacationID;
+    private String vacationTitle;
+    private String lodgingType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 }
