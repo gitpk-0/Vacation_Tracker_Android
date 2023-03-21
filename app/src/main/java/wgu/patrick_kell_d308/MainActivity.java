8ef7c2b40ab503cbda3af475931bc73c96f0d8a2
gitpk-0 <kell108p@protonmail.com>
2023-03-21 10:33:38 -0400

    initial

---
+++ app/src/main/java/wgu/patrick_kell_d308/MainActivity.java
@@ -0,0 +1,14 @@
+package wgu.patrick_kell_d308;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
