1bd4ea3f96d67b18f6ea9eeb9fdeec6db00e2657
gitpk-0 <kell108p@protonmail.com>
2023-03-21 19:54:42 -0400

    B1 - database start

---
+++ app/src/main/java/wgu/patrick_kell_d308/Database/Repository.java
@@ -0,0 +1,7 @@
+package wgu.patrick_kell_d308.Database;
+
+/**
+ * @author Patrick Kell
+ */
+public class Repository {
+}
---
+++ app/src/main/java/wgu/patrick_kell_d308/Database/VacationDatabaseBuilder.java
@@ -0,0 +1,40 @@
+package wgu.patrick_kell_d308.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import wgu.patrick_kell_d308.DAO.ExcursionDAO;
+import wgu.patrick_kell_d308.DAO.VacationDAO;
+import wgu.patrick_kell_d308.Entities.Excursion;
+import wgu.patrick_kell_d308.Entities.Vacation;
+
+/**
+ * @author Patrick Kell
+ */
+
+// change version any time a change is made to either Entity class
+@Database(entities = {Vacation.class, Excursion.class}, version = 1, exportSchema = false)
+public abstract class VacationDatabaseBuilder extends RoomDatabase {
+
+    public abstract VacationDAO vacationDAO();
+
+    public abstract ExcursionDAO excursionDAO();
+
+    private static volatile VacationDatabaseBuilder INSTANCE;
+
+    static VacationDatabaseBuilder getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (VacationDatabaseBuilder.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), VacationDatabaseBuilder.class, "VacationDatabase.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
