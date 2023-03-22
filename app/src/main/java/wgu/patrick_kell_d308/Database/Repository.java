bbb02651eec7f24fc8313ee042d8b9c237ac6413
gitpk-0 <kell108p@protonmail.com>
2023-03-22 08:59:44 -0400

    B1 - Repository infrastructure in place

--- app/src/main/java/wgu/patrick_kell_d308/Database/Repository.java
+++ app/src/main/java/wgu/patrick_kell_d308/Database/Repository.java
@@ -1,7 +1,124 @@
 package wgu.patrick_kell_d308.Database;
 
+import android.app.Application;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import wgu.patrick_kell_d308.DAO.ExcursionDAO;
+import wgu.patrick_kell_d308.DAO.VacationDAO;
+import wgu.patrick_kell_d308.Entities.Excursion;
+import wgu.patrick_kell_d308.Entities.Vacation;
+
 /**
  * @author Patrick Kell
  */
 public class Repository {
+
+    private VacationDAO mVacationDAO; // m's in front of instance variables, s's in front of static variables
+    private ExcursionDAO mExcursionDAO;
+    private List<Vacation> mAllVacations;
+    private List<Excursion> mAllExcursions;
+
+    private static int NUMBER_OF_THREADS;
+    static final ExecutorService databaseExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public Repository(Application application) {
+        VacationDatabaseBuilder db = VacationDatabaseBuilder.getDatabase(application);
+        mVacationDAO = db.vacationDAO();
+        mExcursionDAO = db.excursionDAO();
+    }
+
+    public List<Vacation> getAllVacations() {
+        databaseExecutor.execute(() -> {
+            mAllVacations = mVacationDAO.getAllVacations();
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return mAllVacations;
+    }
+
+    public void insert(Vacation vacation) {
+        databaseExecutor.execute(() -> {
+            mVacationDAO.insert(vacation);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Vacation vacation) {
+        databaseExecutor.execute(() -> {
+            mVacationDAO.update(vacation);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Vacation vacation) {
+        databaseExecutor.execute(() -> {
+            mVacationDAO.delete(vacation);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Excursion> getAllExcursions() {
+        databaseExecutor.execute(() -> {
+            mAllExcursions = mExcursionDAO.getAllExcursions();
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return mAllExcursions;
+    }
+
+    public void insert(Excursion excursion) {
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.insert(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Excursion excursion) {
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.update(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Excursion excursion) {
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.delete(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
